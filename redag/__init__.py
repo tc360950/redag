a3d4bf1bb72258794a287441d7a2e25fb3ea5730
tcakala <tcakala@deloittece.com>
2022-04-29 11:22:32 +0200

    Added some minor comments, proper setup, example now imports from installed Redag package

---
+++ redag/__init__.py
@@ -0,0 +1,6 @@
+from redag.redag import REDAG
+from redag.entity.dimension import Dimension, dimension
+from redag.entity.fact import fact
+from redag.entity.entity import generator_decorator, multiplicity_generator_decorator
+from redag.formatter import SampleFormatter
+from redag.entity.redag_types import Reference
